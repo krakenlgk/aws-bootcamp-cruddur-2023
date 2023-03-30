61c46c260052200a19367f72baa78e8a5a9c086b
Karthikeyan <lgkarthikeyan94@gmail.com>
2023-03-30 13:59:38 +0000

    verify JWT service code

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -50,8 +50,8 @@ origins = [frontend, backend]
 cors = CORS(
   app, 
   resources={r"/api/*": {"origins": origins}},
-  expose_headers="location,link",
-  allow_headers="content-type,if-modified-since",
+  headers=['Content-Type', 'Authorization'], 
+  expose_headers='Authorization',
   methods="OPTIONS,GET,HEAD,POST"
 )
 
@@ -92,6 +92,10 @@ def data_create_message():
 
 @app.route("/api/activities/home", methods=['GET'])
 def data_home():
+  print ('AUTH HEADER------')
+  print (
+    request.headers.get('Authorization')
+  )
   data = HomeActivities.run()
   return data, 200
 
